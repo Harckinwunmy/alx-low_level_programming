c58faf40e30025493ed1e79dcab9a61de0ab262d
Harckinwunmy <spikwitme@gmail.com>
2022-10-17 16:27:26 -0700

    print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -11,19 +11,14 @@ int main(void)
 
 {
 
-	int n = 48;
+	int num;
 
-	while (n <= 57)
+	for (num = 0; num < 10; num++)
 
-	{
+		putchar((num % 10) + '0');
 
-		putchar(n);
 
-		n += 1;
-
-	}
-
-	putchar("\n");
+	putchar('\n');
 
 	return (0);
 
