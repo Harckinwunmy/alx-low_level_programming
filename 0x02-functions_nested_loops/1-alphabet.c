5ab91b87df9ce8bbb6fb78a85887fcd94072db62
Harckinwunmy <spikwitme@gmail.com>
2022-10-23 18:01:36 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print alphabet - print the alphabet in lower case
+ */
+void print_alphabet(void)
+{
+	char dean;
+	for (dean = 'a'; dean <= 'z'; dean++)
+		_putchar(dean);
+	_putchar('\n')
+}
