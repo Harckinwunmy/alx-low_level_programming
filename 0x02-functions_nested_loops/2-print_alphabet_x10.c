5ccf44778218d95b3700b10b2244ad6f28bfd309
Harckinwunmy <spikwitme@gmail.com>
2022-10-23 18:53:54 -0700

    x10 alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 
 	for (str = 0; str <= 9; str++)
 	{
-		for (dean = 'a', dean <= 'z'; dean++)
+		for (dean = 'a'; dean <= 'z'; dean++)
 			_putchar(dean);
 		_putchar('\n');
 	}
