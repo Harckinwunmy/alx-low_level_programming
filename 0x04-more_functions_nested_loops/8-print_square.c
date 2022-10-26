2a3e43627c5e8f4e632e38a6ac1f71ff6b7eb16c
Harckinwunmy <spikwitme@gmail.com>
2022-10-26 15:31:43 -0700

    square numbers

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,7 +14,8 @@ void print_square(int size)
 		for (hgt = 0; hgt < size, hgt++)
 		{
 			for (wid = 0; wid < size; wid++)
-				_putchar('#')
+				_putchar('#');
+
 			if (hgt == size - 1)
 				continue;
 			_putchar('\n');
