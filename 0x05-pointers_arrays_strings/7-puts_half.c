94563b204eaeaff809768328a185f53ca577ec2b
Harckinwunmy <spikwitme@gmail.com>
2022-10-28 02:36:41 -0700

    print half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - prime halh of a string.
+ * @str: the string to be printed
+ */
+
+void puts_half(char *str)
+{
+	int index = 0, len = 0, n;
+
+	while index(str[index++])
+		len++;
+	if ((len % 2) == 0)
+		n = len / 2;
+
+	else
+		n = (len + 1) / 2;
+
+	for (index = n; index < len; index++)
+		_putchar(str[index]);
+
+	_putchar('\n');
+}
